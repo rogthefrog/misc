5b052f7a0e3531fbf56ec24748589173f57f2121
rogthefrog <rogthefrog@gmail.com>
2012-07-24 15:55:11 -0700

    splitting the size into width and height

--- file_sizes/file_sizes.rb
+++ file_sizes/file_sizes.rb
@@ -1,7 +1,7 @@
 File.open('file_sizes.txt', 'r').each_line do |l|
   # 03012_treesbythewater_1680x1050.jpeg-sample/2490x1867.png - 7654305
-  bits = l.split(/\/([0-9x]+).+?(\d+)$/).collect! { |x| x.strip }
-  puts "%s,%s" % [ bits[1], bits[2] ] unless bits[1].nil?
+  bits = l.split(/\/([0-9]+)x([0-9]+)\D.+?(\d+)$/).collect! { |x| x.strip }
+  puts "%s,%s,%s" % [ bits[1], bits[2], bits[3] ] unless bits[1].nil?
 end
 
   
